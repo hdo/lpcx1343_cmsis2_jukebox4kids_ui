f676f2255ef7b4f8c43a2642b28004e7fddecc79
Huy Do <huy.do@gmx.de>
2013-04-22 14:03:44 +0200

    added defines for button indexes

--- LPCX1343_cmsis2_jukebox4kids_ui/src/buttons.h
+++ LPCX1343_cmsis2_jukebox4kids_ui/src/buttons.h
@@ -1,11 +1,15 @@
 #ifndef __BUTTONS_H
 #define __BUTTONS_H
 
-#define BUTTON0 (1 << 1)
-#define BUTTON1 (1 << 2)
-#define BUTTON2 (1 << 3)
-#define BUTTON3 (1 << 6)
+#define BUTTON0 (1 << 1) // ON/OFF
+#define BUTTON1 (1 << 2) // PREV
+#define BUTTON2 (1 << 3) // PLAY/PAUSE
+#define BUTTON3 (1 << 6) // NEXT
 
+#define BUTTON_ON_OFF 0
+#define BUTTON_PREV 1
+#define BUTTON_PLAY_PAUSE 2
+#define BUTTON_NEXT 3
 
 void buttons_init(void);
 uint32_t buttons_read_status(void);
